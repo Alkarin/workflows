42e7d0ecd449b739e13951ba997710144ef61e11
AlexanderV <avaughan@barefootsolutions.com>
2016-09-16 17:12:57 -0700

    added task functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
-    concat = require('gulp-concat'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    sassR = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
+    compass = require('gulp-compass'),
+    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
   'components/scripts/rclick.js',
   'components/scripts/pixgrid.js',
   'components/scripts/tagline.js',
-  'components/scripts/template.js',
+  'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -19,9 +23,31 @@ gulp.task('coffee', function() {
     .pipe(gulp.dest('components/scripts'));
 });
 
-gulp.task('js', function(){
+gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'));
+});
+
+
+gulp.task('default', ['coffee','js','compass']);
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      css: 'css',
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'))
+});
+
+gulp.task('sass', function(){
+  return gulp.src(sassSources)
+    .pipe(sass()) // Using gulp-sass
+    .on('error', gutil.log)
+    .pipe(gulp.dest('builds/development/css'))
 });
